1279a704caf313f0c31b02d7458f49a44c7d0f47
ernesthenry <henry38ernest@gmail.com>
2019-07-26 05:15:51 -0400

    Add views, urls and homepage template file

--- library/catalog/urls.py
+++ library/catalog/urls.py
@@ -1,7 +1,8 @@
 # Use include() to add paths from the catalog application 
 from django.urls import include
 from django.urls import path
+from . import views
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
 ]
--- library/library/urls.py
+++ library/library/urls.py
@@ -7,9 +7,13 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+    # path('catalog/', include('catalog.urls')),
+    # path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
 
