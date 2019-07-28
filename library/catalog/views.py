374d47cec292e87a1a5b8146b22272e5ee4e2df9
ernesthenry <henry38ernest@gmail.com>
2019-07-28 13:02:10 -0400

    Modify form file

--- library/catalog/views.py
+++ library/catalog/views.py
@@ -1,27 +1,29 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
 from django.views import generic
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
 # Create your views here.
+
 def index(request):
     """View function for home page of site."""
 
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-    
+
     # Available books (status = 'a')
-    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-    
-    # The 'all()' is implied by default.    
+    num_instances_available = BookInstance.objects.filter(
+        status__exact='a').count()
+
+    # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
     # Number of visits to this view, as counted in the session variable.
     num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits + 1
 
-    
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
@@ -36,13 +38,14 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
-    paginate_by = 1
+    paginate_by = 5
     # context_object_name = 'my_book_list'   # your own name for the list as a template variable
     # queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
     # template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
 
     def get_queryset(self):
-        return Book.objects.filter(title__icontains='on')[:5] # Get 5 books containing the title war
+        # Get 5 books containing the title war
+        return Book.objects.filter(title__icontains='on')[:5]
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get the context
@@ -51,5 +54,25 @@ class BookListView(generic.ListView):
         context['some_data'] = 'This is just some data'
         return context
 
+
 class BookDetailView(generic.DetailView):
-    model = Book   
+    model = Book
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 5
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+class LoanedBooksAllListView(PermissionRequiredMixin, generic.ListView):
+    """Generic class-based view listing all books on loan. Only visible to users with can_mark_returned permission."""
+    model = BookInstance
+    permission_required = 'catalog.can_mark_returned'
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
