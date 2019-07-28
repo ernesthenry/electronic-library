47542ff360c29eb9929ce336e0abfc57caaedef0
ernesthenry <henry38ernest@gmail.com>
2019-07-28 05:48:15 -0400

    Add authentication and permissions

--- library/library/settings.py
+++ library/library/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'library.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates/library'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 SESSION_SAVE_EVERY_REQUEST = True
+LOGIN_REDIRECT_URL = '/'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
