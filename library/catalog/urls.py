20324fbc3fd7ce4a61afdcb8a26ceb07172b0618
ernesthenry <henry38ernest@gmail.com>
2019-07-29 13:55:01 -0400

    Add authors to the website Delete, create, update authors

--- library/catalog/urls.py
+++ library/catalog/urls.py
@@ -7,12 +7,27 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('books/catalog', views.BookListView.as_view(), name='books'),
     path('book/catalog/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>',
+         views.AuthorDetailView.as_view(), name='author-detail'),
 ]
 
 urlpatterns += [
-    path('catalog/mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    path('catalog/borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),  # Added for challenge
+    path('catalog/mybooks/', views.LoanedBooksByUserListView.as_view(),
+         name='my-borrowed'),
+    path('catalog/borrowed/', views.LoanedBooksAllListView.as_view(),
+         name='all-borrowed'),  # Added for challenge
 ]
-urlpatterns += [   
-    path('catalog/book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+urlpatterns += [
+    path('catalog/book/<uuid:pk>/renew/',
+         views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+urlpatterns += [
+    path('catalog/author/create/',
+         views.AuthorCreate.as_view(), name='author_create'),
+    path('catalog/author/<int:pk>/update/',
+         views.AuthorUpdate.as_view(), name='author_update'),
+    path('catalog/author/<int:pk>/delete/',
+         views.AuthorDelete.as_view(), name='author_delete'),
 ]
--- library/catalog/views.py
+++ library/catalog/views.py
@@ -8,7 +8,9 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from catalog.models import Author
 from catalog.forms import RenewBookForm
 
 # Create your views here.
@@ -42,6 +44,17 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class AuthorListView(generic.ListView):
+    """Generic class-based list view for a list of authors."""
+    model = Author
+    paginate_by = 10
+
+
+class AuthorDetailView(generic.DetailView):
+    """Generic class-based detail view for an author."""
+    model = Author
+
 
 
 class BookListView(generic.ListView):
@@ -119,3 +132,19 @@ def renew_book_librarian(request, pk):
     }
 
     return render(request, 'catalog/book_renew_librarian.html', context)
+
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = '__all__'
+    initial = {'date_of_death': '05/01/2018'}
+
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
